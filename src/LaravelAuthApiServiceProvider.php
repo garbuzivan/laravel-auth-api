b05ecb40232cc0ce8e6ef14b21b00f8e3d628788
Garbuz Ivan <yanzlatov@gmail.com>
2021-01-25 10:51:58 +0300

    Update

---
+++ src/LaravelAuthApiServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GarbuzIvan\LaravelAuthApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImageManagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services...
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
