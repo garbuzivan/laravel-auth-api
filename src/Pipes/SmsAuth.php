27f538ae4830cf0a1939869c8a28dd591fa5947d
Garbuz Ivan <yanzlatov@gmail.com>
2021-01-27 21:20:40 +0300

    Update

--- src/Pipes/SmsAuth.php
+++ src/Pipes/SmsAuth.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace GarbuzIvan\LaravelAuthApi\Pipes;
 
+use App\Models\User;
 use GarbuzIvan\LaravelAuthApi\AuthStatus;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Prozorov\DataVerification\Exceptions\LimitException;
+use Prozorov\DataVerification\Exceptions\VerificationException;
 use Prozorov\DataVerification\Types\Address;
 
 class SmsAuth extends AbstractPipes
@@ -28,17 +33,19 @@ class SmsAuth extends AbstractPipes
      */
     public function authBySmsStep1(AuthStatus $auth): AuthStatus
     {
-        $arg = $auth->getArg();
-        $manager = app('otp');
-        $address = new Address($arg['phone']);
-        $otp = $manager->generateAndSend($address, 'sms');
-        $status = [
-            'step'  =>  'step2',
-            'code'  =>  $otp->getVerificationCode(),
-            'phone' =>  $arg['phone'],
-            'pass'  =>  false,
-        ];
-        $auth->setStatus($status);
+        if (isset($arg['phone']) && !is_null($arg['phone'])) {
+            $arg = $auth->getArg();
+            $manager = app('otp');
+            $address = new Address($arg['phone']);
+            $otp = $manager->generateAndSend($address, 'sms');
+            $status = [
+                'step' => 'step2',
+                'code' => $otp->getVerificationCode(),
+                'phone' => $arg['phone'],
+                'pass' => false,
+            ];
+            $auth->setStatus($status);
+        }
         return $auth;
     }
 
@@ -48,7 +55,18 @@ class SmsAuth extends AbstractPipes
      */
     public function authBySmsStep2(AuthStatus $auth): AuthStatus
     {
-        $arg = $auth->getArg();
+        if (isset($arg['phone']) && isset($arg['code']) && isset($arg['pass'])) {
+            $arg = $auth->getArg();
+            $manager = app('otp');
+            try {
+                $manager->verifyOrFail($arg['code'], $arg['pass']);
+            } catch (\OutOfBoundsException|VerificationException|LimitException $e) {
+                $auth->setError($e->getMessage());
+            }
+            $newToken = Str::random(80);
+            User::where('id', Auth::user()->id)->update(['api_token' => $newToken]);
+            $auth->setToken($newToken);
+        }
         return $auth;
     }
 }
