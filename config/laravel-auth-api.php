d7b0f516f35831f43a818f723afd65adf6343f3d
Garbuz Ivan <yanzlatov@gmail.com>
2021-01-30 12:38:37 +0300

    Update

--- config/laravel-auth-api.php
+++ config/laravel-auth-api.php
@@ -16,4 +16,10 @@ return [
     // Length token
     'length_token' => 80,
 
+    // Email generator code
+    'code_email' => [
+        'charset'   =>  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        'length'    =>  4,
+    ],
+
 ];
