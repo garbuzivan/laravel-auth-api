d8694f0c4a18660206356e212b76f64952db10d4
Garbuz Ivan <yanzlatov@gmail.com>
2021-02-02 12:46:04 +0300

    update

--- resources/views/Mail.blade.php
+++ resources/views/Mail.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>{{ $details['title'] }}</title>
+    <title>{{ $details['title'] ?? null }}</title>
 </head>
 <body>
-<h1>{{ $details['title'] }}</h1>
-<p>{{ $details['body'] }}</p>
+<h1>{{ $details['title'] ?? null }}</h1>
+<p>{{ $details['body'] ?? null }}</p>
 </body>
 </html>
